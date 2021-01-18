bb019dda61dd474a7177a33f23b29387126ef8d0
alfiecri <jediknightalfie@gmail.com>
2021-01-18 11:54:11 +0800

    Initial commit

---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    public float playerSpeed;
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+            transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+        }
+    }
+}
